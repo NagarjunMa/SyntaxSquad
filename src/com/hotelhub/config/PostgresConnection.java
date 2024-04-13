1e647057c2e50a3bd53c36a1e628fcb9b3b25643
ChannaveerS <lnu.cha@northeastern.edu>
2024-04-13 13:19:41 -0400

    UI fixed and hibernate error fixed for my local

--- src/com/hotelhub/config/PostgresConnection.java
+++ src/com/hotelhub/config/PostgresConnection.java
@@ -8,17 +8,17 @@ import com.hotelhub.hibernate.SessionManager;
 
 public class PostgresConnection {
 
-	public static Connection Connector() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/hotelhub", "postgres", "");
-            return conn;
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
-        }
-    }
-	
+//	public static Connection Connector() {
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/hotelhub", "postgres", "123123");
+//            return conn;
+//        } catch (Exception e) {
+//            System.out.println(e);
+//            return null;
+//        }
+//    }
+//	
 	
 	public static void checkConnection() {
 	    Session session = SessionManager.openSession();
