14e64c214d6f98ffc9d4edb4f7c97a3e7d12c6f4
ChannaveerS <lnu.cha@northeastern.edu>
2024-04-16 21:59:00 -0400

    User dashboard and book room UI

---
+++ src/com/hotelhub/controllers/BookRoomController.java
@@ -0,0 +1,12 @@
+package com.hotelhub.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+
+public class BookRoomController {
+
+ 
+}
